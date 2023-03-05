9bf54b76fddd5bfecbb6438aded96552753fa244
Ksenia Sycheva <k.sycheva@student.tudelft.nl>
2023-03-05 16:24:30 +0100

    update: interfaces

--- lib/interfaces.py
+++ lib/interfaces.py
@@ -3,11 +3,13 @@ import board
 import busio
 import adafruit_bmp280
 import digitalio
+import storage
+import sdcardio
 
 
 def _get_SPI_pins(data):
     pins = data['SPI']
-    return getattr(board, pins['SCK']), getattr(board, pins['MISO']), getattr(board, pins['MOSI'])
+    return getattr(board, pins['SCKL']), getattr(board, pins['MISO']), getattr(board, pins['MOSI'])
 
 
 def _get_I2C_pins(data):
@@ -29,9 +31,9 @@ class Interface:
         scl, sda = _get_I2C_pins(data)
         tx, rx = _get_UART_pins(data)
 
-        self.SPI = busio.SPI(sck, MISO=miso, MOSI=mosi)
-        self.I2C = busio.I2C(scl, sda)
-        self.UART = busio.UART(tx, rx)
+        self.SPI = busio.SPI(sck, MISO=miso, MOSI=mosi) # SPI for SD, Lora transceiver
+        self.I2C = busio.I2C(scl, sda)                  # I2C for BMP280
+        self.UART = busio.UART(tx, rx)                  # UART for GPS
 
         self.bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(self.I2C)
         self.bmp280.sea_level_pressure = 1013.25
@@ -41,6 +43,15 @@ class Interface:
         self.lora_cs.direction = digitalio.Direction.OUTPUT
         self.lora_cs.value = True
 
+        out = getattr(board, data['microSD']['cs'])
+        self.microSD_cs = digitalio.DigitalInOut(out)
+        self.sdcard = sdcardio.SDCard(self.SPI, self.microSD_cs)
+        self.vfs = storage.VfsFat(self.sdcard)
+        storage.mount(self.vfs, "/sd")
+
+        self.counter = 0
+        self.buffer = bytearray()
+
     def _read_bmp280_data(self):
         return self.bmp280.temperature, self.bmp280.pressure, self.bmp280.altitude
 
@@ -49,6 +60,9 @@ class Interface:
         self.lora_cs.value = False
         self.SPI.write([t, p, a])
         self.lora_cs.value = True
+
+    def measure(self):
+        pass
 
 
 
